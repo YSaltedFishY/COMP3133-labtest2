471d98652dcbcf312e47efffa519d764f573eab3
YSaltedFishY <yxwyyhy@hotmail.com>
2025-04-02 13:49:38 -0400

    deployment

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,11 @@ import { AppComponent } from './app.component';
 import { MissionlistComponent } from './missionlist/missionlist.component';
 import { MissiondetailsComponent } from './missiondetails/missiondetails.component';
 import { MissionfilterComponent } from './missionfilter/missionfilter.component';
+import { RenderMode } from '@angular/ssr';
 
 export const routes: Routes = [
     {path: '', component: MissionlistComponent},
-    {path: 'detail/:id', component: MissiondetailsComponent},
+    {path: 'detail/:id', component: MissiondetailsComponent, data: { prerender: false }},
     {path: 'filter', component: MissionfilterComponent}
 ];
 
